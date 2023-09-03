03f9cac8b7cfa7bed0822f703163455b7d09caae
AkashSaha747 <akashkaliyaganj5@gmail.com>
2023-09-03 16:58:42 +0530

    corefare-assignment

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,30 +1,23 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import corefare from '../assets/corefares.jpg'
-import './Navbar.css';
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
-function ColorSchemesExample() {
-    return (
-        <>
-            <Navbar bg="dark" data-bs-theme="dark" collapseOnSelect className='navbar'  >
-                <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    </Nav>
-                    <div>
-                        <img src={corefare} width={'90px'} height={'70px'} style={{ padding: '0 9px' }} className='image' />
-                    </div>
-                </Container>
-
-
-            </Navbar>
-
-        </>
-    );
+function MyNavbar() {
+  return (
+    <Navbar bg="dark" expand="lg" variant="dark">
+      <Container>
+     
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#services">Services</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default ColorSchemesExample;
+export default MyNavbar;
